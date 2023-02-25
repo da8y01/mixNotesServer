45b151284171098c859ca2d8e8c9d55421eaec0b
Developer <developer@local.host>
2023-02-24 21:07:23 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb+srv://user1:user1user1@cluster0.rddsa6m.mongodb.net/mixedNotes?retryWrites=true&w=majority'
+    'mongoUrl' : 'mongodb+srv://user1:user1user1@cluster0.rddsa6m.mongodb.net/mixedNotes?retryWrites=true&w=majority',
+    'facebook': {
+        clientId: '749694806716371',
+        clientSecret: 'fdd40ae86ee43bb63b163d40854a0fc3'
+    }
 }
